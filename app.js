9eb17c6881769f4d78118ff5cfebe3ce0b5bbca4
Habib Lomma <102693211+urshabib@users.noreply.github.com>
2026-06-11 15:56:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ async function claimLink() {
                 <span class="success-badge">✓ Account Provisioned</span>
                 <div class="link-display" id="generatedLinkText">${result.url}</div>
                 <div class="action-row">
-                    <button class="btn-copy" onclick="copyToClipboard()" style="width: 100%;">Copy Link</button>
+                    <button class="btn-copy" onclick="window.open('${result.url}', '_blank')" style="width: 100%;">Open Link</button>
                 </div>
             `;
             
